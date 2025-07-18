a4f56849463ec734c08d5ef1cb55c11f1c4c601e
rinevard <rinevard@outlook.com>
2025-07-18 13:33:52 +0800

    目录圆角

--- themes/wikitten-new/source/js/main.js
+++ themes/wikitten-new/source/js/main.js
@@ -1,8 +1,8 @@
-(function($){
+(function ($) {
     // 初始化变量
     // Caption
-    $('.article-entry').each(function(i) {
-        $(this).find('img').each(function() {
+    $('.article-entry').each(function (i) {
+        $(this).find('img').each(function () {
             if (this.alt && !(!!$.prototype.justifiedGallery && $(this).parent('.justified-gallery').length)) {
                 $(this).after('<span class="caption">' + this.alt + '</span>');
             }
@@ -17,7 +17,7 @@
         var options = {
             selector: '.gallery-item',
         };
-        $('.article-entry').each(function(i, entry) {
+        $('.article-entry').each(function (i, entry) {
             lightGallery(entry, options);
         });
         lightGallery($('.article-gallery')[0], options);
@@ -42,12 +42,12 @@
     });
 
     // 回到顶部按钮逻辑已移至layout.ejs中
-    
+
     // Task lists in markdown
-    $('.article-entry ul > li').each(function() {
+    $('.article-entry ul > li').each(function () {
         var taskList = {
             field: this.textContent.substring(0, 2),
-            check: function(str) {
+            check: function (str) {
                 var re = new RegExp(str);
                 return this.field.match(re);
             }
@@ -59,7 +59,7 @@
         function update(str, check) {
             var click = ["disabled", ""];
             $current.html($current.html().replace(
-              str, "<input type='checkbox' " + check + " " + click[1] + " >")
+                str, "<input type='checkbox' " + check + " " + click[1] + " >")
             )
         }
         if (checked || unchecked) {
